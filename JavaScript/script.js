5ae344f3010949fe31d434e2d3dae425fe59e992
Caleb Georgeson <geo16004@byui.edu>
2019-09-27 10:50:10 -0600

    change to JavaScript/script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,5 +2,8 @@ function drawRect() {
    var canvas = document.getElementById("myCanvas");
    var ctx = canvas.getContext("2d");
    ctx.fillStyle = "#FF0000";
-   ctx.fillRect(0, 0, 80, 80);
+   var i = 0;
+   for (i; i < 500; i++) {
+      ctx.fillRect(0, 0, i, 80);
+   }
 }
