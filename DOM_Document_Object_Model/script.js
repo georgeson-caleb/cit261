3a0d0d083782f3c4c521f1ef63ea3636735bf13e
Caleb Georgeson <geo16004@byui.edu>
2019-10-01 19:05:25 -0600

    more dom manipulation

--- DOM_Document_Object_Model/script.js
+++ DOM_Document_Object_Model/script.js
@@ -1,6 +1,20 @@
 function appendChildExample() {
    var node = document.createElement("LI");
-   var textNode = document.createTextNode("boat");
+   var textNode = document.createTextNode("Boat");
    node.appendChild(textNode);
    document.getElementById("stuff").appendChild(node);
+}
+
+function insertBeforeExample() {
+   var node = document.createElement("LI");
+   var textNode = document.createTextNode("Plane");
+   node.appendChild(textNode);
+   document.getElementById("stuff").insertBefore(textNode, stuff[1]);
+}
+
+function removeChildExample() {
+   var item = document.getElementById("stuff");
+   if (item.hasChildNodes()) {
+      item.removeChild(item[0]);
+   }
 }
