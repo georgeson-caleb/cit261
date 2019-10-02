f0cce4902928805d29cf1bd5d71a0b9254995cc7
Caleb Georgeson <geo16004@byui.edu>
2019-10-01 19:20:50 -0600

    getting closer

--- DOM_Document_Object_Model/script.js
+++ DOM_Document_Object_Model/script.js
@@ -10,7 +10,7 @@ function insertBeforeExample() {
    var textNode = document.createTextNode("Plane");
    node.appendChild(textNode);
    var list = document.getElementById("stuff");
-   console.out(list.childNodes[1].value);
+   console.log(list.childNodes[1].value);
    list.insertBefore(node, list.childNodes[1]);
 }
 
