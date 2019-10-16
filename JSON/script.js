b57faf3d3d82f731c4df0ca279317ae7550627f8
Caleb Georgeson <geo16004@byui.edu>
2019-10-16 17:22:52 -0600

    finished with JSON

--- JSON/script.js
+++ JSON/script.js
@@ -1,3 +1,5 @@
+var parsedObject;
+
 class Object {
    name;
    shape;
@@ -5,21 +7,38 @@ class Object {
    things;
 }
 
-function parseObject() {
+function stringifyObject() {
    var object = new Object();
    object.name = document.getElementById("name").value;
    object.shape = document.getElementById("shapes").value;
    object.color = document.getElementById("color").value;
    object.things = new Array();
 
-   //Add the things to the list
-   var checkBox = document.getElementById("checkBox");
-   for (var i = 0; i < checkBox.childNodes.length; i++) {
-      if (checkBox.childNodes[i].childNodes[0].checked) {
-         object.things.push(checkBox.childNodes[i].chileNodes[0].value);
-      }
+   if (document.getElementById("checkbox1").checked) {
+      object.things.push(document.getElementById("checkbox1").value);
    }
+   if (document.getElementById("checkbox2").checked) {
+      object.things.push(document.getElementById("checkbox2").value);
+   }
+   if (document.getElementById("checkbox3").checked) {
+      object.things.push(document.getElementById("checkbox3").value);
+   }
+   if (document.getElementById("checkbox4").checked) {
+      object.things.push(document.getElementById("checkbox4").value);
+   }
+   
+   parsedObject = JSON.stringify(object);
+   document.getElementById("stringified").innerHTML = parsedObject;
+   document.getElementById("parseButton").style.visibility = "visible";
+}
 
-   var parsedObject = JSON.parse(object);
-   document.getElementById("objectPlace").innerHTML = parsedObject;
+function parseObject() {
+   var object = JSON.parse(parsedObject);
+   var displayString = "Name: " + object.name + "<br>Shape: " +
+                        object.shape + "<br>Color: " + object.color +
+                        "<br>Things:";
+   for (var i = 0; i < object.things.length; i++) {
+      displayString += " " + object.things[i];
+   }
+   document.getElementById("parsed").innerHTML = displayString;
 }
