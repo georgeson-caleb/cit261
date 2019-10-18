be301f176d97cc8df0ddd2c5f8685332b81852c1
Caleb Georgeson <geo16004@byui.edu>
2019-10-18 12:41:05 -0600

    Adding css/javascript project

---
+++ CSS3_using_JavaScript/script.js
@@ -0,0 +1,9 @@
+var iBorder = 0;
+
+function changeBorder() {
+   var borders = ["2px solid black", "5px dotted red", "10px ridged blue"]
+
+   var selection = iBorder % borders.length;
+
+   document.getElementById("example").style.border = borders[selection];
+}
