51447f39909db420ee266f447d9bbb0942454c5f
Caleb Georgeson <geo16004@byui.edu>
2019-11-09 21:39:25 -0700

    LocalStorage example

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -9,8 +9,8 @@ function storeData(key, data) {
    localStorage.setItem(key, data);
 }
 
-function retrieveData(key) {
-   key = document.getElementById("key").value;
+function retrieveData() {
+   var key = document.getElementById("key").value;
    if (localStorage.getItem(key) != null) {
       showData(localStorage.getItem(key));
    } else {
@@ -37,4 +37,29 @@ function showData(dataString) {
 
 function showNoKeyError() {
    document.getElementById("thing").innerHTML = "There is no item with that name."
+}
+
+function saveNumber() {
+   storeData("number", document.getElementById("number").value);
+}
+
+function loadNumber() {
+   document.getElementById("loadedNumber").innerHTML = localStorage.getItem("number");
+}
+
+function saveArray() {
+   var animals = [];
+   for (var i = 1; i <= 5; i++) {
+      var string = "array" + i;
+      var checkbox = document.getElementById(string);
+      if (checkbox.checked) {
+         animals.push(checkbox.value);
+      }
+   }
+   //var jsonAnimals = JSON.stringify(animals);
+   localStorage.setItem("animals", animals);
+}
+
+function loadArray() {
+   document.getElementById("loadedArray").innerHTML = localStorage.getItem("animals");
 }
