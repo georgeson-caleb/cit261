37380782ab49f28919f29b1281130af05bad9f51
Caleb Georgeson <geo16004@byui.edu>
2019-12-11 22:24:13 -0700

    final project updates

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -5,6 +5,57 @@ class Student {
    description;
 }
 
+var Monday = false;
+var Tuesday = false;
+var Wednesday = false;
+var Thursday = false;
+var Friday = false;
+
+function selectDay(elem) {
+   var day = elem.innerHTML;
+   if (day == "Monday") {
+      if (Monday == true) {
+         Monday = false;
+         deselect(elem);
+      } else {
+         Monday = true;
+         select(elem);
+      }
+   } else if (day == "Tuesday") {
+      if (Tuesday == true) {
+         Tuesday = false;
+         deselect(elem)
+      } else {
+         Tuesday = true;
+         select(elem)
+      }         
+   } else if (day == "Wednesday") {
+      if (Wednesday == true) {
+         Wednesday = false;
+         deselect(elem)
+      } else {
+         Wednesday = true;
+         select(elem)
+      }         
+   } else if (day == "Thursday") {
+      if (Thursday == true) {
+         Thursday = false;
+         deselect(elem)
+      } else {
+         Thursday = true;
+         select(elem)
+      }      
+   } else if (day == "Friday") {
+      if (Friday == true) {
+         Friday = false;
+         deselect(elem)
+      } else {
+         Friday = true;
+         select(elem)
+      }      
+   }
+}
+
 function play(note) {
    document.getElementById(note).play();
 }
@@ -46,10 +97,27 @@ function showPage() {
 function slide1Left() {
    document.getElementById("slide1").classList = "slideLeftDisappear";
    document.getElementById("slide2").style.display = "block";
-   document.getElementById("slide2").classList = "slideLeftAppear";
+   document.getElementById("slide2").classList += " slideLeftAppear";
 }
 
-function slide2Left() {
-   document.getElementById("slide2").style.display = "block";
-   document.getElementById("slide2").classList = "slideLeftAppear";
+function wiggleDiv(elem) {
+   elem.classList += " wiggle";
+}
+
+function undoWiggle(elem) {
+   elem.classList.remove("wiggle");
+}
+
+function select(elem) {
+   elem.style.backgroundColor = "#d64e4e";
+   elem.style.borderColor = "#fa5757";
+   elem.style.width = "70%";
+   elem.style.height = "40px";
+}
+
+function deselect(elem) {
+   elem.style.backgroundColor = "#fa5757";
+   elem.style.borderColor = "#d64e4e";
+   elem.style.width = "50%";
+   elem.style.height = "30px";
 }
