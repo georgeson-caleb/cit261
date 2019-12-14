26029d45b72d781c09bfaedf04773613ec46a0d8
Caleb Georgeson <geo16004@byui.edu>
2019-12-13 19:58:06 -0700

    Added documentation

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,3 +1,8 @@
+
+/*****************
+ * Student class
+ * Used to group student data together
+ *****************/
 class Student {
    firstName;
    lastName;
@@ -13,6 +18,11 @@ var Wednesday = false;
 var Thursday = false;
 var Friday = false;
 
+/*********************
+ * Select day
+ * Determines what happens when a 
+ * day gets clicked
+ *********************/
 function selectDay(elem) {
    var day = elem.innerHTML;
    if (day == "Monday") {
@@ -59,6 +69,11 @@ function selectDay(elem) {
 }
 
 var time = '';
+/*************************
+ * Select time
+ * Determines what happens when a 
+ * time gets clicked
+ */
 function selectTime(elem) {
    var times = document.getElementById("timeSelection").childNodes;
    
@@ -71,18 +86,35 @@ function selectTime(elem) {
    time = elem.innerHTML;
 }
 
+/*******************
+ * Play
+ * Plays the passed note
+ *******************/
 function play(note) {
    document.getElementById(note).play();
 }
 
+/*********************
+ * Disable
+ * Makes the element disappear
+ *********************/
 function disable(elem) {
    elem.style.display = "none";
 }
 
+/**********************
+ * Enable
+ * Makes the element appear
+ **********************/
 function enable(elem) {
    elem.style.display = "block";
 }
 
+/***********************
+ * Register
+ * Assigns each of the variables 
+ * from the input boxes to the student
+ ***********************/
 function register() {
    var student = new Student();
    student.firstName = document.getElementById("firstName").value;
@@ -94,6 +126,11 @@ function register() {
    saveStudentInfo(student);
 }
 
+/***********************
+ * Get Days
+ * Checks which days have been 
+ * selected.
+ ***********************/
 function getDays() {
    var days = [];
    if (Monday) {
@@ -115,6 +152,11 @@ function getDays() {
    return days;
 }
 
+/***************************
+ * saveStudentInfo
+ * Sends the student's info to the 
+ * server to be processed.
+ ***************************/
 function saveStudentInfo(student) {
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
@@ -127,12 +169,24 @@ function saveStudentInfo(student) {
    xhttp.send(JSON.stringify(student));
 }
 
+/**************************
+ * Start loading transition
+ * First transition:
+ * A div slides up and becomes 
+ * less transparent
+ **************************/
 function startLoadingTransition() {
    var obj = document.getElementById("loadingScreen");
    obj.style.opacity = "100%";
    obj.style.transform = "translate(50%, 200px)";
 }
 
+/**************************
+ * showPage
+ * When the first transition ends,
+ * this function is called to make
+ * the rest of the page appear
+ **************************/
 function showPage() {
    document.getElementById("loadingScreen").style.display = "none";
    document.getElementById("piano").style.display = "flex";
@@ -140,20 +194,40 @@ function showPage() {
    document.getElementById("header").style.display = "block";
 }
 
+/***************************
+ * Slide1 Left
+ * Triggers the first animation:
+ * Slide1 disappears to the left 
+ * while Slide2 appears from the 
+ * right
+ ***************************/
 function slide1Left() {
    document.getElementById("slide1").classList = "slideLeftDisappear";
    document.getElementById("slide2").style.display = "block";
    document.getElementById("slide2").classList += " slideLeftAppear";
 }
 
+/***************************
+ * wiggleDiv
+ * Makes the passed element wiggle
+ ***************************/
 function wiggleDiv(elem) {
    elem.classList += " wiggle";
 }
 
+/****************************
+ * undoWiggle
+ * Allows the element to wiggle again
+ ****************************/
 function undoWiggle(elem) {
    elem.classList.remove("wiggle");
 }
 
+/*****************************
+ * select
+ * Triggers another transition:
+ * The element changes size and color
+ *****************************/
 function select(elem) {
    elem.style.backgroundColor = "#d64e4e";
    elem.style.borderColor = "#fa5757";
@@ -161,6 +235,11 @@ function select(elem) {
    elem.style.height = "40px";
 }
 
+/*****************************
+ * deselect
+ * The element returns to its 
+ * original style
+ *****************************/
 function deselect(elem) {
    elem.style.backgroundColor = "#fa5757";
    elem.style.borderColor = "#d64e4e";
@@ -168,16 +247,27 @@ function deselect(elem) {
    elem.style.height = "30px";
 }
 
+/******************************
+ * updateResponse
+ * Fills in the response after the user
+ * submits their information
+ ******************************/
 function updateResponse(response) {
    document.getElementById("firstNameResponse").innerHTML = response.firstName;
    document.getElementById("timeResponse").innerHTML = response.time;
    var dayString = "";
    for (var i = 0; i < response.days.length; i++) {
-      dayString += (i < (response.days.length - 1)) ? response.days[i] + ", " : response.days[i];
+      dayString += (i < (response.days.length - 1)) ? response.days[i] + ", " : "and " + response.days[i];
    }
    document.getElementById("dayResponse").innerHTML = dayString;
 }
 
+/*******************************
+ * showResponse
+ * Triggers another animation:
+ * Slide2 disappears to the left
+ * while slide3 appears from the right
+ *******************************/
 function showResponse() {
    document.getElementById("slide2").classList += " slideLeftDisappear";
    document.getElementById("slide2").addEventListener("animationend", function() {
